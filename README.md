4bdaa48c99c602eaf77ba5da15d63cf75e2a8e68
Felix Abecassis <felix.abecassis@gmail.com>
2017-12-15 19:29:08 -0800

    Initial commit
    Signed-off-by: Felix Abecassis <felix.abecassis@gmail.com>

---
+++ README.md
@@ -0,0 +1,83 @@
+# go-gunzip-bench
+
+Benchmark multiple ways of decompressing a `gzip` file in golang.
+
+## Preparation
+```sh
+$ go build -ldflags="-s -w"
+
+$ mkdir -p tmp
+
+$ sudo mount -t tmpfs tmpfs tmp/
+
+$ wget -nv https://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/cudnn-9.0-linux-x64-v7.tgz -O tmp/cudnn.tgz
+2017-12-15 18:03:22 URL:https://developer.download.nvidia.com/compute/redist/cudnn/v7.0.5/cudnn-9.0-linux-x64-v7.tgz [348817823/348817823] -> "tmp/cudnn.tgz" [1]
+
+$ grep -m 1 'model name' /proc/cpuinfo
+model name      : Intel(R) Core(TM) i7-5930K CPU @ 3.50GHz
+
+# Note: we use `runtime.GOMAXPROCS(1)`
+$ echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+performance
+```
+
+## [Method 0](https://github.com/flx42/go-gunzip-bench/blob/master/main.go#L25-L39)
+Don't use "compress/gzip", pipe to gunzip(1)
+```sh
+$ ./go-gunzip-bench 0 tmp/cudnn.tgz
+tmp/cudnn.tgz:  4.426812529s
+
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## [Method 1](https://github.com/flx42/go-gunzip-bench/blob/master/main.go#L41-L56)
+Chain two readers, low memory usage, most idiomatic solution
+```sh
+$ ./go-gunzip-bench 1 tmp/cudnn.tgz
+tmp/cudnn.tgz:  7.84219111s
+
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## [Method 2](https://github.com/flx42/go-gunzip-bench/blob/master/main.go#L58-L72)
+Read the whole file in-memory, stream decompress/write to output file.
+```
+$ ./go-gunzip-bench 2 tmp/cudnn.tgz
+tmp/cudnn.tgz:  7.692913915s
+
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## [Method 3](https://github.com/flx42/go-gunzip-bench/blob/master/main.go#L75-L87)
+Read the whole file in-memory, and decompress the whole file in-memory.
+```sh
+$ ./go-gunzip-bench 3 tmp/cudnn.tgz
+tmp/cudnn.tgz:  7.941778804s
+
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## [Method 4](https://github.com/flx42/go-gunzip-bench/blob/master/main.go#L89-L104)
+Method 1 but using [cgzip](https://github.com/youtube/vitess/tree/master/go/cgzip), a golang wrapper for [zlib](https://www.zlib.net) (using cgo).
+```sh
+$ ./go-gunzip-bench 4 tmp/cudnn.tgz
+tmp/cudnn.tgz:  3.403516352s
+
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## [`gunzip(1)`](https://www.gnu.org/software/gzip/manual/gzip.html)
+`gunzip(1)` for read, decompress, write.
+```sh
+$ /usr/bin/time gunzip --keep --force tmp/cudnn.tgz && sha256sum tmp/cudnn.tar
+4.21user 0.21system 0:04.42elapsed 99%CPU (0avgtext+0avgdata 1788maxresident)k
+0inputs+0outputs (0major+157minor)pagefaults 0swaps
+36b429f6f780ab46d6dfd5888918968cd5882ef6b6f4cbd97d596a2da211a4c7  tmp/cudnn.tar
+```
+
+## Cleanup
+```sh
+$ sudo umount tmp/
+
+$ rm go-gunzip-bench
+```
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"github.com/youtube/vitess/go/cgzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Panicln("Fatal:", err)
+	}
+}
+
+// Method 0: don't use "compress/gzip", pipe to gunzip(1)
+func method0(dst, src string) {
+	f, err := os.Open(src)
+	check(err)
+	defer f.Close()
+
+	w, err := os.Create(dst)
+	check(err)
+	defer w.Close()
+
+	cmd := exec.Command("gunzip")
+	cmd.Stdin = f
+	cmd.Stdout = w
+	check(cmd.Run())
+}
+
+// Method 1: chain two readers, low memory usage, most idiomatic solution
+func method1(dst, src string) {
+	f, err := os.Open(src)
+	check(err)
+	defer f.Close()
+
+	r, err := gzip.NewReader(f)
+	check(err)
+
+	w, err := os.Create(dst)
+	check(err)
+	defer w.Close()
+
+	_, err = io.Copy(w, r)
+	check(err)
+}
+
+// Method 2: read the whole file in-memory, stream decompress/write to output file.
+func method2(dst, src string) {
+	fb, err := ioutil.ReadFile(src)
+	check(err)
+
+	r, err := gzip.NewReader(bytes.NewReader(fb))
+	check(err)
+
+	w, err := os.Create(dst)
+	check(err)
+	defer w.Close()
+
+	_, err = io.Copy(w, r)
+	check(err)
+}
+
+// Method 3: read the whole file in-memory, and decompress the whole file in-memory.
+func method3(dst, src string) {
+	fb, err := ioutil.ReadFile(src)
+	check(err)
+
+	r, err := gzip.NewReader(bytes.NewReader(fb))
+	check(err)
+
+	rb, err := ioutil.ReadAll(r)
+	check(err)
+
+	err = ioutil.WriteFile(dst, rb, 0666)
+	check(err)
+}
+
+// Method 4: Method 1 but using cgzip, a golang wrapper for zlib (using cgo).
+func method4(dst, src string) {
+	f, err := os.Open(src)
+	check(err)
+	defer f.Close()
+
+	r, err := cgzip.NewReader(f)
+	check(err)
+
+	w, err := os.Create(dst)
+	check(err)
+	defer w.Close()
+
+	_, err = io.Copy(w, r)
+	check(err)
+}
+
+func warmPageCache(path string) {
+	f, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0)
+	check(err)
+	defer f.Close()
+
+	cmd := exec.Command("cat", path)
+	cmd.Stdout = f
+	check(cmd.Run())
+}
+
+func sha256sum(path string) {
+	cmd := exec.Command("sha256sum", path)
+	cmd.Stdout = os.Stdout
+	check(cmd.Run())
+}
+
+func main() {
+	// Just in case, to avoid too much interference from the go runtime.
+	runtime.GOMAXPROCS(1)
+	if len(os.Args) != 3 {
+		os.Exit(2)
+	}
+
+	method, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		os.Exit(2)
+	}
+
+	src := os.Args[2]
+	dst := strings.Replace(src, ".tgz", ".tar", -1)
+
+	// cat src > /dev/null
+	warmPageCache(src)
+
+	start := time.Now()
+	switch method {
+	case 0:
+		method0(dst, src)
+	case 1:
+		method1(dst, src)
+	case 2:
+		method2(dst, src)
+	case 3:
+		method3(dst, src)
+	case 4:
+		method4(dst, src)
+	default:
+		os.Exit(2)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("%s:  %s\n\n", src, elapsed)
+
+	sha256sum(dst)
+}
